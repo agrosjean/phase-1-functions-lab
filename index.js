eb5947a77dc66c79640838d4aa122df8f78603db
alexandre <agrosjean007@gmail.com>
2021-10-13 14:25:42 -0400

    Assignment completed funciton lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customer1) {
+    return (Math.abs(customer1 - 42));
+}
+
+function distanceFromHqInFeet(customer1) {
+    return (Math.abs(customer1 - 42) * 264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return (Math.abs(start - end) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    const dist = distance - 400;
+
+    if (distance < 400) {
+        return 0;
+    }
+    else if (distance > 400 && distance < 2000) {
+        return dist * 0.02;
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+
+}
